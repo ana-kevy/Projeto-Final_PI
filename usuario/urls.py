729ef16f4ca60880c7e030e77884532918649452
Salesvani-o <jrrmatias123@gmail.com>
2025-10-12 21:32:54 -0300

    aalteracoes

--- usuario/urls.py
+++ usuario/urls.py
@@ -5,11 +5,12 @@ app_name = 'usuario'
 
 urlpatterns = [
     # Usuários
-    path('', views.UsuarioListView.as_view(), name='usuario_list'),
+    path('u', views.UsuarioListView.as_view(), name='usuario_list'),
     path('novo/', views.UsuarioCreateView.as_view(), name='usuario_create'),
     path('<int:pk>/', views.UsuarioDetailView.as_view(), name='usuario_detail'),
     path('<int:pk>/editar/', views.UsuarioUpdateView.as_view(), name='usuario_update'),
     path('<int:pk>/excluir/', views.UsuarioDeleteView.as_view(), name='usuario_delete'),
+    path('', views.login, name='login'),
 
     # Mensagens
     path('mensagens/', views.MensagemListView.as_view(), name='mensagem_list'),
--- usuario/views.py
+++ usuario/views.py
@@ -2,7 +2,8 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Usuario, Mensagem
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 
 # ----- Usuário -----
 class UsuarioListView(LoginRequiredMixin, ListView):
@@ -51,3 +52,29 @@ class MensagemCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.candidato = self.request.user
         return super().form_valid(form)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('listar_vagas')
+    
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Bem-vindo, {user.username}!')
+                
+                # Redireciona para a página solicitada ou para listar_vagas
+                next_page = request.GET.get('next', 'listar_vagas')
+                return redirect(next_page)
+        else:
+            messages.error(request, 'Usuário ou senha inválidos.')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'usuarios/login.html', {'form': form})
