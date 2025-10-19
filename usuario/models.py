4388e4a1d48074cb8f511170460b0ae661b04ca3
ana-kevy <anakevyllycavalcantec@gmail.com>
2025-10-19 20:17:04 -0300

    models

--- usuario/models.py
+++ usuario/models.py
@@ -1,13 +1,15 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
 class UsuarioAdaptado(AbstractUser):
     cpf = models.CharField(max_length=14, unique=True, null=True, blank=True)
     endereco = models.CharField(max_length=255, blank=True, null=True)
     curriculo = models.FileField(upload_to='curriculos/', blank=True, null=True)
     habilidades = models.TextField(blank=True, null=True)
+    link_portfolio = models.URLField(blank=True, null=True)
+    email = models.EmailField(unique=True)
     is_candidato = models.BooleanField(default=False)
     is_empresa = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
 
     def is_gerente(self):
         return self.groups.filter(name='GERENTE').exists()
@@ -17,3 +19,4 @@ class UsuarioAdaptado(AbstractUser):
 
     def is_candidato(self):
         return self.groups.filter(name='CANDIDATO').exists()
+
